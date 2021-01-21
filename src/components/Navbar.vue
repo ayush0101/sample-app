01719a9c81d76c6ba5b94268f3c235e482140768
Vivekanand P V <vivekanandpv@outlook.com>
2021-01-21 10:28:52 +0530

    active links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,23 +3,23 @@
     <span class="navbar-brand mb-0 h1">Learning Vue.js</span>
     <ul class="navbar-nav">
       <li class="nav-item">
-        <router-link to="/" class="nav-link active">
-          <span>Home</span>
+        <router-link to="/" exact-active-class="active">
+          <span class="nav-link">Home</span>
         </router-link>
       </li>
       <li class="nav-item">
-        <router-link to="/about" class="nav-link">
-          <span>About</span>
+        <router-link to="/about" exact-active-class="active">
+          <span class="nav-link">About</span>
         </router-link>
       </li>
       <li class="nav-item">
-        <router-link to="/contact" class="nav-link">
-          <span>Contact</span>
+        <router-link to="/contact" exact-active-class="active">
+          <span class="nav-link">Contact</span>
         </router-link>
       </li>
       <li class="nav-item">
-        <router-link :to="url" class="nav-link">
-          <span>Playground</span>
+        <router-link :to="url" exact-active-class="active">
+          <span class="nav-link">Playground</span>
         </router-link>
       </li>
     </ul>
