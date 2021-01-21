1a90ae3ce104eb2674378aa1f74724012217ca90
Vivekanand P V <vivekanandpv@outlook.com>
2021-01-21 10:19:19 +0530

    dynamic links

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/> -->
-
+    <Navbar />
     <div class="container p-5">
       <h3>My Vue.js App</h3>
       <hr />
@@ -13,6 +13,15 @@
     </div>
   </div>
 </template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 
 <style>
 /* @import url("https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"); */
---
+++ src/components/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <span class="navbar-brand mb-0 h1">Learning Vue.js</span>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <router-link to="/" class="nav-link active">
+          <span>Home</span>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/about" class="nav-link">
+          <span>About</span>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/contact" class="nav-link">
+          <span>Contact</span>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link :to="url" class="nav-link">
+          <span>Playground</span>
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      link: "/playground",
+      id: 1245,
+      city: "Tumkur",
+      qp: {
+        lang: "kn",
+        country: "india",
+      },
+    };
+  },
+  computed: {
+    url() {
+      return `${this.link}/${this.id}/${this.city}?${this.toQueryString}`;
+    },
+    toQueryString() {
+      let queryString = "";
+      for (const key in this.qp) {
+        if (queryString.length > 0) {
+          queryString += "&";
+        }
+        if (Object.hasOwnProperty.call(this.qp, key)) {
+          queryString += `${key}=${this.qp[key]}`;
+        }
+      }
+
+      return queryString;
+    },
+  },
+};
+</script>
